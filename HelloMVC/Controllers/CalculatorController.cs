734bd421f13ea1cb76900d427fc8bf48ae7a295b
nhkhanh <nguyenhuykhanh@gmail.com>
2017-02-23 16:40:12 +0700

    First commit

---
+++ HelloMVC/Controllers/CalculatorController.cs
@@ -0,0 +1,25 @@
+﻿using HelloMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sum(int a, int b)
+        {
+            CalculatorModels model = new CalculatorModels() { a = a, b = b };
+            model.Sum();
+            return View(model);
+        }
+    }
+}
---
+++ HelloMVC/Models/CalculatorModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloMVC.Models
+{
+    public class CalculatorModels
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+        public int s { get; set; }
+
+        public int Sum()
+        {
+            return s = a + b;
+        }
+    }
+}
